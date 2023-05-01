00c274e20cd675141ef6cd91d570994b8596f01b
Parsa Yazdani <3parsa3@gmail.com>
2023-05-01 21:20:11 +1000

    Logout

--- admin/inc/header.inc.php
+++ admin/inc/header.inc.php
@@ -1,6 +1,5 @@
 <div class="flex-shrink-0 bg-zinc-200 w-full h-16">
      <ul class="flex items-center justify-end h-full">
-        <li class="px-3 text-black ">Login</li>
-        <li class="px-3 text-balck">Register</li>
+        <li class="px-3 text-balck"><a href="/admin/logout">Logout</a></li>
     </ul>
-</div> 
+</div>
