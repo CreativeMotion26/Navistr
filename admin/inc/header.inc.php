0b4f9bfae5ed514f0d4d3bed3fbc0aa60d3cadd5
Min <kjm4540@gmail.com>
2023-05-01 19:56:20 +1000

    dsfjsdlk

--- admin/inc/header.inc.php
+++ admin/inc/header.inc.php
@@ -1,6 +1,6 @@
 <div class="flex-shrink-0 bg-gray-800 w-full h-16">
      <ul class="flex items-center justify-end h-full">
-        <li class="px-3 text-white ">Login</li>
+        <li class="px-3 text-black ">Login</li>
         <li class="px-3 text-white">Register</li>
     </ul>
 </div> 
