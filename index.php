3e553679f65848aa906039e2cc340d130695caad
CreativeMotion26 <kjm4540@gmail.com>
2023-05-01 20:34:21 +1000

    absolute

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <section class="py-0 container mx-auto bg-white py-8 border-t border-gray-400">
             <div class="container py-5">
             <div class="PageWidth PageWidth--large relative" style="--mobileContainerHeight:150px; --desktopContainerHeight:120px;">
-            <div class="media absolute inset-0 flex justify-center items-center"></div><div class="textContentContainer z-10 w-full max-w-full justify-center">
+            <div class="media inset-0 flex justify-center items-center"></div><div class="textContentContainer z-10 w-full max-w-full justify-center">
                 <div class="textContent-cover textContent-background py-16 flex flex-col items-center" style="--textImageMaxWidthMobile:300px; --textImageMaxWidthDesktop:800px;">
                 <h4 class="text-orange-500 font-semibold text-sm mb-2 uppercase tracking-widest">Reliable Classics</h4>
                 <h1 class="text-4xl md:text-5xl font-bold mb-4">About Florentine</h1>
@@ -29,7 +29,7 @@
         <section class="py-0 container mx-auto bg-white py-8 border-t border-orange-400">
             <div class="ProductDetailsUniversal-container ProductDetailsUniversal--mobile py-8">
             <div class="PageWidth ProductDetailsUniversal ProductDetailsUniversal--noBackgroundContainer PageWidth--large relative" style="--mobileContainerHeight:150px; --desktopContainerHeight:120px;">
-            <div class="ProductDetailsUniversal-media absolute inset-0 flex justify-center items-center"></div><div class="ProductDetailsUniversal-textContentContainer z-10 w-full max-w-full justify-center">
+            <div class="ProductDetailsUniversal-media inset-0 flex justify-center items-center"></div><div class="ProductDetailsUniversal-textContentContainer z-10 w-full max-w-full justify-center">
                <div class="ProductDetailsUniversal-textContent ProductDetailsUniversal-textContent-cover ProductDetailsUniversal-textContent-background py-16 flex flex-col items-center" style="--textImageMaxWidthMobile:300px; --textImageMaxWidthDesktop:800px;">
                 <h4 class="text-orange-500 font-semibold text-sm mb-2 uppercase tracking-widest">Reliable Classics</h4>
                 <h1 class="text-4xl md:text-5xl font-bold mb-4">Florentine Essentials</h1>
@@ -42,27 +42,27 @@
         <section>
              <div class="grid grid-cols-2 gap-4">
                 <a href="/pages/new-gear" class="relative h-48 bg-center bg-cover" style="background-image: url('/assets/images/item1.jpg')">
-                    <div class="absolute inset-0 bg-black opacity-50"></div>
-                    <h1 class="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold">New Gear</h1>
+                    <div class="inset-0 bg-black opacity-50"></div>
+                    <h1 class="inset-0 flex items-center justify-center text-white text-4xl font-bold">New Gear</h1>
                 </a>
                 <a href="/collections/cases" class="relative h-48 bg-center bg-cover" style="background-image: url('https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto,c_limit,w_3448,h_4229/https://f.shgcdn.com/2b0be81e-9142-4da4-9921-9fcc2f63804a/')">
-                    <div class="absolute inset-0 bg-black opacity-50"></div>
-                    <h1 class="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold">Cases</h1>
+                    <div class="inset-0 bg-black opacity-50"></div>
+                    <h1 class="inset-0 flex items-center justify-center text-white text-4xl font-bold">Cases</h1>
                 </a>
                 <a href="/products/sport-slim-band-sage-41mm" class="relative h-48 bg-center bg-cover" style="background-image: url('https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto,c_limit,w_6000,h_4000/https://f.shgcdn.com/113f00ee-1924-41d0-8492-bf82b53e32ba/')">
-                    <div class="absolute inset-0 bg-black opacity-50"></div>
-                    <h1 class="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold">Sport Bands</h1>
+                    <div class="inset-0 bg-black opacity-50"></div>
+                    <h1 class="inset-0 flex items-center justify-center text-white text-4xl font-bold">Sport Bands</h1>
                 </a>
                 <a href="/collections/power" class="relative h-48 bg-center bg-cover" style="background-image: url('https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto,c_limit,w_3448,h_4229/https://f.shgcdn.com/db675c9d-db3a-42ec-8ed3-f738cac89365/')">
-                    <div class="absolute inset-0 bg-black opacity-50"></div>
-                    <h1 class="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold">Power</h1>
+                    <div class="inset-0 bg-black opacity-50"></div>
+                    <h1 class="inset-0 flex items-center justify-center text-white text-4xl font-bold">Power</h1>
                 </a>
             </div>
         </section>
         <section class="py-0 container mx-auto bg-white py-8 border-t border-orange-400">
             <div class="ProductDetailsUniversal-container ProductDetailsUniversal--mobile py-8">
             <div class="PageWidth ProductDetailsUniversal ProductDetailsUniversal--noBackgroundContainer PageWidth--large relative" style="--mobileContainerHeight:150px; --desktopContainerHeight:120px;">
-            <div class="ProductDetailsUniversal-media absolute inset-0 flex justify-center items-center"></div><div class="ProductDetailsUniversal-textContentContainer z-10 w-full max-w-full justify-center">
+            <div class="ProductDetailsUniversal-media inset-0 flex justify-center items-center"></div><div class="ProductDetailsUniversal-textContentContainer z-10 w-full max-w-full justify-center">
                <div class="ProductDetailsUniversal-textContent ProductDetailsUniversal-textContent-cover ProductDetailsUniversal-textContent-background py-16 flex flex-col items-center" style="--textImageMaxWidthMobile:300px; --textImageMaxWidthDesktop:800px;">
                 <h4 class="text-orange-500 font-semibold text-sm mb-2 uppercase tracking-widest">Reliable Classics</h4>
                 <h1 class="text-4xl md:text-5xl font-bold mb-4">Florentine Essentials</h1>
@@ -80,7 +80,7 @@
                     <img class="w-full h-full object-cover"
                     src="assets/images/item5.jpg"
                     alt="30W Power Adapter"/>
-                    <img class="w-full h-full object-cover absolute top-0 left-0 opacity-0 hover:opacity-100 transition-opacity duration-300"
+                    <img class="w-full h-full object-cover top-0 left-0 opacity-0 hover:opacity-100 transition-opacity duration-300"
                     src="assets/items/item6.jpg"
                     alt="30W Power Adapter"/>
                 </div>
@@ -101,7 +101,7 @@
                     src="https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto,c_limit,w_3840,h_3840/https://f.shgcdn.com/195d5b3f-a923-4d8d-8544-5690dc5978be/"
                     alt="Rugged Case"/>
                     <img
-                    class="w-full h-full object-cover absolute top-0 left-0 opacity-0 hover:opacity-100 transition-opacity duration-300"
+                    class="w-full h-full object-cover top-0 left-0 opacity-0 hover:opacity-100 transition-opacity duration-300"
                     src="https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto,c_limit,w_3840,h_3840/https://f.shgcdn.com/7b6bf555-3945-479c-9567-67632494555d/"
                     alt="Rugged Case"/>
                 </div>
@@ -122,7 +122,7 @@
                     src="https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto,c_limit,w_3840,h_3840/https://f.shgcdn.com/195d5b3f-a923-4d8d-8544-5690dc5978be/"
                     alt="Rugged Case"/>
                     <img
-                    class="w-full h-full object-cover absolute top-0 left-0 opacity-0 hover:opacity-100 transition-opacity duration-300"
+                    class="w-full h-full object-cover top-0 left-0 opacity-0 hover:opacity-100 transition-opacity duration-300"
                     src="https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto,c_limit,w_3840,h_3840/https://f.shgcdn.com/7b6bf555-3945-479c-9567-67632494555d/"
                     alt="Rugged Case"/>
                 </div>
@@ -142,7 +142,7 @@
         <section class="py-0">
             <div class="ProductDetailsUniversal-container ProductDetailsUniversal--mobile py-8">
             <div class="PageWidth ProductDetailsUniversal ProductDetailsUniversal--noBackgroundContainer PageWidth--large relative" style="--mobileContainerHeight:150px; --desktopContainerHeight:120px;">
-            <div class="ProductDetailsUniversal-media absolute inset-0 flex justify-center items-center"></div><div class="ProductDetailsUniversal-textContentContainer z-10 w-full max-w-full justify-center">
+            <div class="ProductDetailsUniversal-media inset-0 flex justify-center items-center"></div><div class="ProductDetailsUniversal-textContentContainer z-10 w-full max-w-full justify-center">
                 <div class="ProductDetailsUniversal-textContent ProductDetailsUniversal-textContent-cover ProductDetailsUniversal-textContent-background py-16 flex flex-col items-center" style="--textImageMaxWidthMobile:300px; --textImageMaxWidthDesktop:800px;">
                 <h4 class="text-gray-500 font-semibold text-sm mb-2 uppercase tracking-widest">Reliable Classics</h4>
                 <h1 class="text-4xl md:text-5xl font-bold mb-4">Florentine Product</h1>
