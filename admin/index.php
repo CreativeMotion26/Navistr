613b122c9482c550f8c639df1eeb1fbfccca1dfc
Parsa Yazdani <3parsa3@gmail.com>
2023-05-01 21:04:44 +1000

    flowbite

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,11 @@
 <html lang="en">
     <head>
         <?php require __DIR__ . '/../inc/head.inc.php';?>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet">
     </head>
     <body>
         <?php require __DIR__ . '/inc/header.inc.php';?>
         <?php require __DIR__ . '/inc/sidebar.inc.php';?>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
     </body>
 </html>
