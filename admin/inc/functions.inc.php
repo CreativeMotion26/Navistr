37af62eff92104032d74a295598804b5753aa871
Parsa Yazdani <3parsa3@gmail.com>
2023-05-01 21:05:25 +1000

    db

--- admin/inc/functions.inc.php
+++ admin/inc/functions.inc.php
@@ -1,5 +1,5 @@
 <?php
-//require_once __DIR__ . '/../../inc/functions.inc.php';
+require_once __DIR__ . '/../../inc/functions.inc.php';
 session_start();
 if(!isset($logging_in) && !isset($_SESSION['admin'])){
 //    header('Location: /admin/login');
