d0b464f5b79f552b08c61e1a23ed658ef9550b69
Parsa Yazdani <3parsa3@gmail.com>
2023-05-01 20:44:16 +1000

    login test

--- admin/login.php
+++ admin/login.php
@@ -2,7 +2,7 @@
 $logging_in = true;
 require_once __DIR__ . '/inc/functions.inc.php';
 if(!empty($_POST)){
-    $result = mysqli_query($conn, "SELECT * FROM users WHERE LIMIT 1");
+    $result = mysqli_query(mysqli_fetch_assoc($conn), "SELECT * FROM users WHERE LIMIT 1");
     die(var_export($result, true));
 }
 ?>
