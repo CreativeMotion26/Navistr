bcd0c2e81db95bdd2a4583e587da6a2d3b71d65f
sukush99 <sukushkunwar99@gmail.com>
2023-05-03 21:40:47 +1000

    Update product.php

--- admin/product.php
+++ admin/product.php
@@ -91,6 +91,8 @@
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
+                                    <th scope="col" class="px-4 py-3"><label for="checkbox-table-search-1" class="sr-only">checkbox</label></th>
+                                    <th scope="col" class="px-4 py-3">Product</th>
                                     <th scope="col" class="px-4 py-3">Product name</th>
                                     <th scope="col" class="px-4 py-3">Category</th>
                                     <th scope="col" class="px-4 py-3">Brand</th>
