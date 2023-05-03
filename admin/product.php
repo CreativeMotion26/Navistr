3c46ac9c306b58e5df4d090c2c2b67c2564c19bf
sukush99 <sukushkunwar99@gmail.com>
2023-05-03 21:19:42 +1000

    Update product.php

--- admin/product.php
+++ admin/product.php
@@ -122,11 +122,11 @@
                             <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
                                 $999
                             </td>
-                            <td class="px-2 py-4">
-                                <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline border-2 border-gray-500 rounded-lg bg-gray-100 p-4">Remove</a>
+                            <td class="px-1 py-4">
+                                <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline border-2 border-red-200 rounded-lg bg-gray-100 p-4">Remove</a>
                             </td>
                             <td>
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline border-2 border-gray-500 rounded-lg bg-gray-100 p-4">Edit</a>
+                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline border-2 border-blue-200 rounded-lg bg-gray-100 p-4">Edit</a>
                             </td>
                         </tr>
                     </tbody>
