3558459b3b42e62f131c740984ebea4d91085668
sukush99 <sukushkunwar99@gmail.com>
2023-05-05 18:44:53 +1000

    Update sidebar.inc.php

--- admin/inc/sidebar.inc.php
+++ admin/inc/sidebar.inc.php
@@ -2,8 +2,8 @@
 
 
 <aside id="sidebar-multi-level-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-   <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-      <ul class="space-y-6 font-medium">
+   <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 translate-y-16">
+      <ul class="space-y-4 font-medium">
          
          
          <li>
