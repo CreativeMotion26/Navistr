2e1c5b6fd3cae5c7a2563263056edad454809586
sukush99 <sukushkunwar99@gmail.com>
2023-05-05 19:56:52 +1000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,6 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet">
     </head>
     <body>
-        <?php require __DIR__ . '/inc/header.inc.php';?>
         <?php require __DIR__ . '/inc/sidebar.inc.php';?>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
     </body>
