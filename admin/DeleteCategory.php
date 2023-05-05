c8fbd74633d2dd2527a576243213085e9216eb1a
sukush99 <sukushkunwar99@gmail.com>
2023-05-05 19:22:07 +1000

    Update DeleteCategory.php

--- admin/DeleteCategory.php
+++ admin/DeleteCategory.php
@@ -68,6 +68,9 @@
                 </tbody>
             </table>
         </div>
+        <button class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-blue-500 rounded-lg border border-gray-200 hover:bg-green-500 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
+                                Delete
+        </button>
 
     </form>
 </body>
