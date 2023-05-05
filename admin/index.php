c7ac935ed76fc764a1197d958a00682b518e43be
sukush99 <sukushkunwar99@gmail.com>
2023-05-05 22:20:45 +1000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -45,7 +45,7 @@
                                 </button>
                                 <div id="actionsDropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                                     <div class="py-1">
-                                    <button data-modal-target="defaultModal5" data-modal-toggle="defaultModal5" class="block py-2 px-10 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" type="button">
+                                    <button data-modal-target="defaultModal5" data-modal-toggle="defaultModal5" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" type="button">
                                     Delete Category
                                     </button>
                                     </div>
@@ -283,12 +283,6 @@
                                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                                 <tr>
-                                                    <th scope="col" class="p-4">
-                                                        <div class="flex items-center">
-                                                            <input id="checkbox-all" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                                            <label for="checkbox-all" class="sr-only">checkbox</label>
-                                                        </div>
-                                                    </th>
                                                     <th scope="col" class="px-6 py-3">
                                                         Category Name
                                                     </th>
