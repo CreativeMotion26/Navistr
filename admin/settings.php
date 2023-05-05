50b1bc0ad16f0cb673de2c9d192dff3ddd1a5589
sukush99 <sukushkunwar99@gmail.com>
2023-05-05 22:18:15 +1000

    Update settings.php

---
+++ admin/settings.php
@@ -0,0 +1,10 @@
+<?php require_once __DIR__ . '/inc/functions.inc.php';?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php require __DIR__ . '/../inc/head.inc.php';?>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php require __DIR__ . '/inc/sidebar.inc.php';?>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
