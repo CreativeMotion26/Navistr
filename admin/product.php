1c45aca4d713b3ea9d853aff547a12e716839c00
sukush99 <sukushkunwar99@gmail.com>
2023-05-05 18:53:10 +1000

    Update product.php

--- admin/product.php
+++ admin/product.php
@@ -4,7 +4,7 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet">
 </head>
 <body>
-        <?php require __DIR__ . '/inc/header.inc.php';?>
+        
         <?php require __DIR__ . '/inc/sidebar.inc.php';?>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
         <div class="p-4 sm:ml-64">
