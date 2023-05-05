182ccee6b39fc7d56c6509c6b55c182973127079
sukush99 <sukushkunwar99@gmail.com>
2023-05-05 20:55:39 +1000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -424,7 +424,7 @@
                                             <span class="sr-only">Close modal</span>
                                         </button>
                                     </div>
-                                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                              <!--  <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                             <tr>
                                                 <th scope="col" class="p-4">
@@ -483,7 +483,7 @@
                                                     12
                                             </tr>
                                         </tbody>
-                                    </table>
+                                    </table>-->
 
                                     
                                      Modal footer 
