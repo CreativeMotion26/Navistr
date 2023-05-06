1e4a4450d6fb4fc35a9e33760b27970dde7c36ec
sukush99 <sukushkunwar99@gmail.com>
2023-05-06 22:08:04 +1000

    Update categories.php

---
+++ admin/categories.php
@@ -0,0 +1,11 @@
+<?php require_once __DIR__ . '/inc/functions.inc.php';?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php require __DIR__ . '/../inc/head.inc.php';?>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php require __DIR__ . '/inc/sidebar.inc.php';?>
+    </body>
+</html>
