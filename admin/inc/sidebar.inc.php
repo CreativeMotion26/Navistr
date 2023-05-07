0b02dc67f977eef9aada5200baabd5ab01ecfec6
sukush99 <sukushkunwar99@gmail.com>
2023-05-08 00:29:57 +1000

    Update sidebar.inc.php

--- admin/inc/sidebar.inc.php
+++ admin/inc/sidebar.inc.php
@@ -1,5 +1,5 @@
 
-<div class="border-b border-gray-200 fixed dark:border-gray-700">
+<div class="border-b border-gray-200 dark:border-gray-700">
     <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
         <li class="mr-2">
             <button>
