2311abaa9d5e9467f2ae13777719c295d6b8d212
sukush99 <sukushkunwar99@gmail.com>
2023-05-07 20:38:52 +1000

    Update categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -1,78 +1,60 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Reporting</title>
-	<!-- Import Tailwind CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.17/tailwind.min.css">
-
-	<!-- Custom styles -->
-	<style>
-		/* Set the height of the main container */
-		.main-container {
-			min-height: calc(100vh - 4rem);
-		}
-	</style>
-</head>
-<body>
-	<!-- Navigation bar -->
-	<nav class="bg-gray-900 text-white py-2">
-		<div class="container mx-auto px-4">
-			<a href="#" class="font-bold text-lg">Reporting</a>
-			<ul class="float-right">
-				<li class="inline-block ml-4"><a href="#">Settings</a></li>
-				<li class="inline-block ml-4"><a href="#">Logout</a></li>
-			</ul>
-		</div>
-	</nav>
-
-	<!-- Main content -->
-	<div class="container mx-auto px-4 py-8 main-container">
-		<h2 class="text-lg font-bold mb-4">Sales Report</h2>
-		<div class="bg-white rounded-lg shadow-lg p-4">
-			<div class="flex justify-between items-center mb-4">
-				<div class="text-gray-600">Total Sales</div>
-				<div class="text-green-500">$100,000</div>
-			</div>
-			<div class="flex justify-between items-center mb-4">
-				<div class="text-gray-600"">Total Orders</div>
-                <div class="text-green-500">1,000</div>
-            </div>
-            <div class="flex justify-between items-center mb-4">
-                <div class="text-gray-600">Average Order Value</div>
-                <div class="text-green-500">$100</div>
-            </div>
-            <div class="flex justify-between items-center mb-4">
-                <div class="text-gray-600">Total Customers</div>
-                <div class="text-green-500">500</div>
-            </div>
-        </div>
-        	<h2 class="text-lg font-bold mt-8 mb-4">Sales by Month</h2>
-	    <div class="bg-white rounded-lg shadow-lg p-4">
-                <div class="grid grid-cols-3 gap-4">
-                <div class="text-gray-600">Month</div>
-                <div class="text-gray-600">Sales</div>
-                <div class="text-gray-600">Orders</div>
-                <div>January</div>
-                <div>$10,000</div>
-                <div>100</div>
-                <div>February</div>
-                <div>$12,000</div>
-                <div>120</div>
-                <div>March</div>
-                <div>$15,000</div>
-                <div>150</div>
-                <div>April</div>
-                <div>$18,000</div>
-                <div>180</div>
-                <div>May</div>
-                <div>$20,000</div>
-                <div>200</div>
-                <div>June</div>
-                <div>$22,000</div>
-                <div>220</div>
-            </div>
-	    </div>
-    </div>
-</body>
-</html>
-
+<div class="w-full lg:w-1/2 p-4">
+  <div class="bg-white rounded-lg shadow-lg p-6">
+    <h2 class="text-lg font-bold mb-4">Top Products</h2>
+    <div class="border-b border-gray-200 mb-4"></div>
+    <ul class="list-none">
+      <?php
+        $products = array(
+          array('name' => 'Product A', 'sales' => 150),
+          array('name' => 'Product B', 'sales' => 120),
+          array('name' => 'Product C', 'sales' => 100),
+          array('name' => 'Product D', 'sales' => 80),
+          array('name' => 'Product E', 'sales' => 60),
+        );
+        foreach ($products as $product) {
+          echo '<li class="flex justify-between items-center mb-2">';
+          echo '<div class="text-gray-600">'.$product['name'].'</div>';
+          echo '<div class="text-green-500 font-bold">'.$product['sales'].'</div>';
+          echo '</li>';
+        }
+      ?>
+    </ul>
+  </div>
+</div>
+<div class="w-full lg:w-1/2 p-4">
+  <div class="bg-white rounded-lg shadow-lg p-6">
+    <h2 class="text-lg font-bold mb-4">Sales Graph</h2>
+    <div class="border-b border-gray-200 mb-4"></div>
+    <canvas id="sales-chart" width="400" height="200"></canvas>
+  </div>
+</div>
+<script>
+  var ctx = document.getElementById('sales-chart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+      datasets: [{
+        label: 'Sales',
+        data: [10, 15, 20, 25, 30, 35],
+        backgroundColor: 'rgba(0, 128, 0, 0.2)',
+        borderColor: 'rgba(0, 128, 0, 1)',
+        borderWidth: 1,
+        pointRadius: 3,
+        pointBackgroundColor: 'rgba(0, 128, 0, 1)',
+        pointBorderColor: 'rgba(0, 128, 0, 1)',
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(0, 128, 0, 1)',
+        pointHoverBorderColor: 'rgba(0, 128, 0, 1)',
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
