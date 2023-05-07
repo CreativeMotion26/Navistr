dd7a1971dbe0da02516cbee15dddb3eb59f14fec
sukush99 <sukushkunwar99@gmail.com>
2023-05-08 00:31:24 +1000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -384,7 +384,7 @@
                 <div class="w-full" id="week-signups-chart"></div>
             </div>
             
-            <div class="grid grid-cols-1 my-4 xl:grid-cols-2 xl:gap-4">
+
             <!-- Right Content -->
             <div class="grid gap-4">
                 <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
