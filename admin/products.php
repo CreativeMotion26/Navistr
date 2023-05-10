ce3f8b609af3dbe7c93956bc3728ab5e1cd624a8
Parsa Yazdani <3parsa3@gmail.com>
2023-05-10 22:22:18 +1000

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -21,14 +21,14 @@ if(isset($_POST['add_product'])){
         <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
             <div class="w-full md:w-1/2">
                 <form class="flex items-center">
-                    <label for="simple-search" class="sr-only">Search</label>
+                    <label for="search-bar" class="sr-only">Search</label>
                     <div class="relative w-full">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                             </svg>
                         </div>
-                        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2" placeholder="Search" required="">
+                        <input type="text" id="search-bar" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2" placeholder="Search" required="">
                     </div>
                 </form>
             </div>
@@ -105,7 +105,7 @@ if(isset($_POST['add_product'])){
                     </th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody id="table_content">
                     <?php $result = mysqli_query($conn,"SELECT sku, name, price, description, stock FROM product");
                     while($row = mysqli_fetch_assoc($result)){?>
                         <tr class="border-b">
@@ -346,5 +346,15 @@ if(isset($_POST['add_product'])){
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+    <script>
+        document.getElementById('search-bar').addEventListener('keyup', function(){
+            let search_value = this.value.toLowerCase();
+            document.querySelectorAll('#table_content > tr > :nth-child(4)').forEach(function(e){
+                e.parentElement.style.display = 'table-row'
+                if(!e.innerText.toLowerCase().includes(search_value))
+                    console.log(e.parentElement.style.display = 'none');
+            });
+        });
+    </script>
 </body>
 </html>
