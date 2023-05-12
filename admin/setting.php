a5537b3f108cca0a8db264151bb70ca8b572ba4a
sukush99 <sukushkunwar99@gmail.com>
2023-05-12 19:40:22 +1000

    d

--- admin/setting.php
+++ admin/setting.php
@@ -1,109 +1,134 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require __DIR__ . '/../inc/head.inc.php';?>
+    <meta charset="UTF-8">
+    <title>Title</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet">
 </head>
 <body>
-    <?php require __DIR__ . '/inc/sidebar.inc.php';?>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-            <h2>User Settings</h2>
-            <form>
-                <div class="form-group">
-                <label for="firstName">First Name</label>
-                <input type="text" class="form-control" id="firstName" placeholder="Enter First Name">
-                </div>
-                <div class="form-group">
-                <label for="lastName">Last Name</label>
-                <input type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
-                </div>
-                <div class="form-group">
-                <label for="sex">Sex</label>
-                <select class="form-control" id="sex">
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Other</option>
-                </select>
-                </div>
-                <div class="form-group">
-                <label for="joinedDate">Joined Date</label>
-                <input type="text" class="form-control" id="joinedDate" placeholder="Enter Joined Date">
-                </div>
-                <div class="form-group">
-                <label for="email">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Enter Password">
-                </div>
-                <div class="form-group">
-                <label for="confirmPassword">Confirm Password</label>
-                <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Save Changes</button>
-            </form>
-            </div>
-        </div>
-        
-        <div class="row mt-4">
-            <div class="col-lg-8 mx-auto">
-            <h2>Change Password</h2>
-            <form>
-                <div class="form-group">
-                <label for="currentPassword">Current Password</label>
-                <input type="password" class="form-control" id="currentPassword" placeholder="Enter Current Password">
-                </div>
-                <div class="form-group">
-                <label for="newPassword">New Password</label>
-                <input type="password" class="form-control" id="newPassword" placeholder="Enter New Password">
-                </div>
-                <div class="form-group">
-                <label for="confirmNewPassword">Confirm New Password</label>
-                <input type="password" class="form-control" id="confirmNewPassword" placeholder="Confirm New Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Save Changes</button>
-            </form>
-            </div>
-        </div>
-        
-        <div class="row mt-4">
-            <div class="col-lg-8 mx-auto">
-            <h2>View Other Admin</h2>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#adminModal">
-                View Admin
-            </button>
-            </div>
-        </div>
-        
-        </div>
 
-<!-- Modal -->
-        <div class="modal fade" id="adminModal" tabindex="-1" role="dialog" aria-labelledby="adminModalLabel" aria-hidden="
-            <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="adminModalLabel">Admin Information</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
+
+        <div class="grid grid-cols-1 px-4 pt-6 xl:grid-cols-3 xl:px-0 xl:gap-4 dark:bg-gray-900">
+            <div class="mb-4 col-span-full xl:mb-2">
+                <nav class="flex mb-5" aria-label="Breadcrumb">
+                    <ol class="inline-flex items-center space-x-1 text-sm font-medium md:space-x-2">
+                    <li class="inline-flex items-center">
+                        <a href="#" class="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-primary-500">
+                        <svg class="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                        Home
+                        </a>
+                    </li>
+                    <li>
+                        <div class="flex items-center">
+                        <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <a href="#" class="ml-1 text-gray-700 hover:text-primary-600 md:ml-2 dark:text-gray-300 dark:hover:text-primary-500">Pages</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="flex items-center">
+                        <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <span class="ml-1 text-gray-400 md:ml-2 dark:text-gray-500" aria-current="page">Playground</span>
+                        </div>
+                    </li>
+                    </ol>
+                </nav>
+                <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Create something awesome here</h1>
             </div>
-            <div class="modal-body">
-                <ul>
-                <li><strong>Name:</strong> John Doe</li>
-                <li><strong>Email:</strong> johndoe@example.com</li>
-                <li><strong>Role:</strong> Super Admin</li>
-                </ul>
+            <!-- Right Content -->
+            <div class="col-span-full xl:col-auto">
+                <div class="p-4 mb-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card header</h3>
+                    </div>
+                    <div class="h-32 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card body</h3>
+                    </div>
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card footer</h3>
+                    </div>
+                </div>
+                <div class="p-4 mb-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card header</h3>
+                    </div>
+                    <div class="h-32 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card body</h3>
+                    </div>
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card footer</h3>
+                    </div>
+                </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
+            <div class="col-span-2">
+                <div class="p-4 mb-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card header</h3>
+                    </div>
+                    <div class="h-32 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card body</h3>
+                    </div>
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card footer</h3>
+                    </div>
+                </div>
+                <div class="p-4 mb-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card header</h3>
+                    </div>
+                    <div class="h-32 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card body</h3>
+                    </div>
+                    <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                        <h3>Card footer</h3>
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="grid gap-4 px-4 mb-4 md:grid-cols-2 xl:grid-cols-4 xl:px-0">
+            <div class="p-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card header</h3>
+                </div>
+                <div class="h-16 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card body</h3>
+                </div>
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card footer</h3>
+                </div>
+            </div>
+            <div class="p-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card header</h3>
+                </div>
+                <div class="h-16 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card body</h3>
+                </div>
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card footer</h3>
+                </div>
+            </div>
+            <div class="p-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card header</h3>
+                </div>
+                <div class="h-16 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card body</h3>
+                </div>
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card footer</h3>
+                </div>
+            </div>
+            <div class="p-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card header</h3>
+                </div>
+                <div class="h-16 px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card body</h3>
+                </div>
+                <div class="px-4 py-2 text-gray-400 border border-gray-200 border-dashed rounded dark:border-gray-600">
+                    <h3>Card footer</h3>
+                </div>
+            </div>
         </div>
-    </div>
-    
 </body>
 </html>
