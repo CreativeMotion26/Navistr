185b3224a220c7808e057a2134f9d797a5b422a5
sukush99 <sukushkunwar99@gmail.com>
2023-05-12 20:22:56 +1000

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -14,7 +14,7 @@
 
 
 
-            <div class="w-full max-w-sm bg-white translate-y-16 ">
+            <!--<div class="w-full max-w-sm bg-white translate-y-16 ">-->
                 
                 <div class="flex flex-col items-center pb-10">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="assets\images\contact.jpg" alt="Bonnie image"/>
@@ -25,7 +25,7 @@
                         <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a>
                     </div>
                 </div>
-            </div>
+            
 
 
 
