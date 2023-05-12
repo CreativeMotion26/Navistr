e3947210374f797fe00129c341803b91b5c946ad
sukush99 <sukushkunwar99@gmail.com>
2023-05-12 20:43:35 +1000

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -53,15 +53,12 @@ if(isset($_GET['delete'])){
                     </button>
                
                     </div>
-                    <button class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
+                    <button data-modal-target="defaultModal1" data-modal-toggle="defaultModal1" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
                         <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M10 17.5a7.5 7.5 0 110-15 7.5 7.5 0 010 15zM10 19a9 9 0 100-18 9 9 0 000 18z"/>
                             <path d="M9.293 10.707a1 1 0 000 1.414l2 2a1 1 0 001.414-1.414L11.414 12H15a1 1 0 100-2H11.414l1.293-1.293a1 1 0 10-1.414-1.414l-2 2z"/>
                         </svg>
                         View Category
-
-
-                        
                     </button>
                     <div id="filterDropdown" class="z-10 hidden w-48 p-3 bg-white rounded-lg shadow">
                         <h6 class="mb-3 text-sm font-medium text-gray-900">Choose brand</h6>
@@ -231,68 +228,72 @@ if(isset($_GET['delete'])){
             </div>
         </div>
     </div>
-    <!-- Main modal for Add Products-->
+    <!-- Main modal for view Products-->
 
-    <div id="defaultModal1" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+     <div id="defaultModal1" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative w-full max-w-2xl max-h-full">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow">
                 <!-- Modal header -->
                 <div class="flex items-start justify-between p-4 border-b rounded-t">
-
-                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="defaultModal1">
+                    <h3 class="text-xl font-semibold text-gray-900">
+                        View Category
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="defaultModal5">
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         <span class="sr-only">Close modal</span>
                     </button>
                 </div>
                 <!-- Modal body -->
                 <div class="p-6 space-y-6">
-                    <section class="bg-white">
-                        <div class=" transform -translate-y-16 px-4 mx-auto max-w-2xl lg:py-16">
-                            <h2 class="mb-4 text-xl font-bold text-gray-900">Add a new product</h2>
-                            <form action="/admin/products" method="POST">
-                                <input name="add_product" type="hidden">
-                                <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                                    <div class="sm:col-span-2">
-                                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label>
-                                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type product name" required>
-                                    </div>
-                                    <div class="w-full">
-                                        <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Price</label>
-                                        <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="299.00" required>
-                                    </div>
-                                    <div class="w-full">
-                                        <label for="sku" class="block mb-2 text-sm font-medium text-gray-900">SKU</label>
-                                        <input type="text" name="sku" id="sku" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="SKU" required>
-                                    </div>
-                                    <div>
-                                        <label for="stock" class="block mb-2 text-sm font-medium text-gray-900">stock</label>
-                                        <input type="number" name="stock" id="stock" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="12" required>
-                                    </div>
-                                    <div class="sm:col-span-2">
-                                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Description</label>
-                                        <textarea name="description" id="description" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Your description here"></textarea>
-                                    </div>
-
-                                    <div class="sm:col-span-2">
-                                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Upload Image</label>
-                                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50   hover:bg-gray-100">
-                                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                                <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                                <p class="mb-2 text-sm text-gray-500"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                                                <p class="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                                            </div>
-                                            <input id="dropzone-file" type="file" class="hidden" />
-                                        </label>
-                                    </div>
-
-                                </div>
-                                <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200  hover:bg-primary-800">
-                                    Add product
-                                </button>
-                            </form>
-                        </div>
-                    </section>
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                        <tr>
+                            <th scope="col" class="p-4">
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Category Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Brand
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Type
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class="bg-white border-b   hover:bg-gray-50">
+                            
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                Bags"
+                            </th>
+                            <td class="px-6 py-4">
+                                Latido
+                            </td>
+                            <td class="px-6 py-4">
+                                B-grade
+                            </td>
+                        </tr>
+                        <<tr class="bg-white border-b   hover:bg-gray-50">
+                            
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                Bags"
+                            </th>
+                            <td class="px-6 py-4">
+                                Latido
+                            </td>
+                            <td class="px-6 py-4">
+                                B-grade
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
+                    
+                    <button data-modal-hide="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Close</button>
                 </div>
             </div>
         </div>
