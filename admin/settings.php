03282933430b114cb0030a03ee47a1aa33f96017
sukush99 <sukushkunwar99@gmail.com>
2023-05-12 20:21:44 +1000

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -13,11 +13,11 @@
 
 
 
-           
+
             <div class="w-full max-w-sm bg-white translate-y-16 ">
                 
                 <div class="flex flex-col items-center pb-10">
-                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="assets\images\contact.jpg" alt="Bonnie image"/>
                     <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
                     <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
                     <div class="flex mt-4 space-x-3 md:mt-6">
