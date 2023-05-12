16792cf8bf3ded6799d4f4d4d1e651493236bfb5
sukush99 <sukushkunwar99@gmail.com>
2023-05-12 20:07:08 +1000

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -13,7 +13,10 @@
 
 
 
-            <div class="w-full  text-left max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="w-full  text-left max-w-sd bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-end px-4 pt-4">
+
+                </div>
                 <div class="flex flex-col items-center pb-10">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="assets\images\1.jpg" alt="Bonnie image"/>
                     <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
