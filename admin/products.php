3a03bc2eb222fbec474b7587e1f2f43fa88896d9
Parsa Yazdani <3parsa3@gmail.com>
2023-05-22 13:11:40 +1000

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -117,12 +117,12 @@ if(isset($_POST['add_category'])){
                             <td class="px-4 py-3"><?php echo $row['stock'];?></td>
                             <td class="px-4 py-3">$<?php echo $row['price'];?></td>
                             <td class="px-4 py-3 flex items-center justify-end">
-                                <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" type="button">
+                                <button id="control-<?php echo $row['sku'];?>-dropdown-button" data-dropdown-toggle="control-<?php echo $row['sku'];?>-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" type="button">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                     </svg>
                                 </button>
-                                <div id="apple-imac-27-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow">
+                                <div id="control-<?php echo $row['sku'];?>-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow">
                                     <div class="py-1">
                                         <a href="/admin/addProduct?edit=<?php echo $row['sku'];?>" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Edit</a>
                                     </div>
