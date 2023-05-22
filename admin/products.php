45e3fa808d061a57050e30381b15f6a71a18eb2d
Parsa Yazdani <3parsa3@gmail.com>
2023-05-22 12:31:15 +1000

    Update

--- admin/products.php
+++ admin/products.php
@@ -169,7 +169,18 @@ if(isset($_POST['add_category'])){
                 </div>
                 <!-- Modal body -->
                 <div class="p-6 space-y-6">
-                    <a href="/admin/addCategory" class="inline-flex items-center px-5 py-2.5 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-primary-800">Add Category</a>
+                    <form action="/admin/products" method="POST">
+                        <input name="add_category" type="hidden">
+                        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                            <div class="sm:col-span-2">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Category Name</label>
+                                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type product name" required="">
+                            </div>
+                        </div>
+                        <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200  hover:bg-primary-800">
+                            Add Category
+                        </button>
+                    </form>
                     <table class="w-full text-sm text-left text-gray-500">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                         <tr>
