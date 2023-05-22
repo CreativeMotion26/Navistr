fcf1c854e7a59d7961dd1da2c0f4b625946b55c0
Parsa Yazdani <3parsa3@gmail.com>
2023-05-22 12:42:32 +1000

    Update addProduct.php

--- admin/addProduct.php
+++ admin/addProduct.php
@@ -54,7 +54,7 @@
                                     <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200  hover:bg-primary-800">
                                         Add product
                                     </button>
-                                    <a href="/admin/addProduct" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200  hover:bg-primary-800">
+                                    <a href="/admin/addProduct" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-gray-700 rounded-lg focus:ring-4">
                                         Cancel
                                     </a>
                                 </div>
